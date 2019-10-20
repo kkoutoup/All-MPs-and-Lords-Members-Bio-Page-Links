e0e5a79d35d4d2f2e0afe818ee3de88d3356b977
Kostas Koutoupis <kkoutoup@gmail.com>
2019-10-20 10:58:29 +0100

    Filled in Readme file and edited python file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # All-MPs-and-Lords-Members-Bio-Page-Links
-A Python scraper that collects URLs from Parliament's website for MPs and Lords Members
+
+### Category
+Web scraping/data collection
+
+### Purpose
+Collect URLs from UK Parliament website for all House of Commons MPs and House of Lords Members.
+
+### User needs
+A script developed to help our performance analyst run SEO/Performance checks in batches for all Bio pages (1427 URLs) using Lighthouse. Links to bio pages are collected in .csv file.
+
+### Data collected
+Pages parsed for URLs
+* [URLs to pages for all House of Commons MPs](https://www.parliament.uk/mps-lords-and-offices/mps/)
+* [URLs to pages for all Members of the House of Lords](https://www.parliament.uk/mps-lords-and-offices/lords/)
+
+.csv file contains
+- House name (Commons/Lords)
+- Name of MP/Lords Member
+- Link to Bio Page
+
+### Dependencies
+Built with Python 3.6.4 and the following modules
+- requests_html
+- urllib
+- re
+- time
+- datetime
+- csv
+
+### Developed by
+Kostas Koutoupis [@kkoutoup](https://github.com/kkoutoup) for the Web and Publications Unit (WPU) of the Chambers and Committee Office (CCT), House of Commons
---
+++ all-mps-bio-pages.py
@@ -0,0 +1,69 @@
+#dependencies
+import urllib, re, time, datetime, csv
+from requests_html import HTMLSession
+from urllib.parse import urlparse
+
+#data container
+data = []
+
+#get data function
+def get_data(url):
+  if url.endswith('/mps/'):
+    print("=> Collecting data for House of Commons MPs")
+  else:
+    print("=> Collecting data for House of Lords Members")
+  try:
+    session = HTMLSession()
+    #send request
+    r = session.get(url)
+    #check for errors in request
+    r.raise_for_status()
+  except Exception as e:
+    print(e)
+  else:
+    #get all links from div
+    all_links = r.html.find('#pnlListing', first = True).links
+    #make sure links have correct structure
+    hoc_pattern =  re.compile(r'https://www\.parliament\.uk/biographies/commons/(.*)/\d+')
+    hol_pattern =  re.compile(r'https://www\.parliament\.uk/biographies/lords/(.*)/\d+')
+    #append links to data container
+    for link in all_links:
+      if url.endswith('/mps/'):
+        if hoc_pattern.fullmatch(link) and link not in data:
+          #format name
+          name = (re.match(hoc_pattern, link).group(1).replace('-', ' ')).title()+' MP'
+          #tackle encoding issue for weird characters for name and link and append to list
+          data.append(['House of Commons', urllib.parse.unquote(name, encoding='utf-8'), urllib.parse.unquote(link, encoding='utf-8')])
+      else:
+         if hol_pattern.fullmatch(link) and link not in data:
+           #format name
+           name = (re.match(hol_pattern, link).group(1).replace('-', ' ')).title()
+           #append data to list
+           data.append(['House of Lords', name, link])
+    return data
+
+#write to csv   
+def write_to_csv(data):
+  print("=> Printing to CSV")
+  try:
+    with open('parliament_members_data.csv', 'w') as csv_file:
+      csv_writer = csv.writer(csv_file, lineterminator = '\n')
+      csv_writer.writerow(['House', 'MP/Member Name', 'Biolink'])
+      for item in data:
+        csv_writer.writerow([item[0], item[1], item[2]])
+  except Exception as e:
+    print(e)
+  else:
+    return '=> All done!'
+    
+#set sequence                          
+def sequence():
+  start = time.time()
+  hoc_links = get_data('https://www.parliament.uk/mps-lords-and-offices/mps/')
+  all_links = get_data('https://www.parliament.uk/mps-lords-and-offices/lords/')  
+  result = data.append(write_to_csv(all_links))
+  end = time.time()
+  print(f"=> All done! Process completed in(approx in min/seconds): {str(datetime.timedelta(seconds=end-start))}")
+
+#run sequence
+sequence()
